110095076981cecec11ab5d4c2eb031f3ccea324
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-04-22 15:53:49 -0300

    Adicionar arquivos de projeto.

---
+++ StarbuckXamarin.Android/SplashActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace StarbuckXamarin.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+
+            // Disable activity slide-in animation
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
