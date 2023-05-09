fed0ac1ae6e25c86cb8c3988ab30d516ca9a879c
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-09 20:07:31 -0300

    feat: add and configure fonts

--- StarbuckXamarin/AssemblyInfo.cs
+++ StarbuckXamarin/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "PoppinsRegular")]
+[assembly: ExportFont("ProximaNova-Regular.otf", Alias = "ProximaNova")]
+[assembly: ExportFont("Raleway-Regular.ttf", Alias = "RalewayRegular")]
+[assembly: ExportFont("Raleway-Medium.ttf", Alias = "RalewayMedium")]
