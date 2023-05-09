bb3498e2654f02638284362cc1becaa0a961e6be
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-09 17:18:10 -0300

    feat: add corner radius in tabbar android

---
+++ StarbuckXamarin.Android/Effects/MyShellBottomNavViewAppearanceTracker.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.BottomNavigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Content.Res;
+
+namespace StarbuckXamarin.Droid.Effects
+{
+    class MyShellBottomNavViewAppearanceTracker : IShellBottomNavViewAppearanceTracker
+    {
+        public void Dispose()
+        {
+        }
+
+        public void ResetAppearance(BottomNavigationView bottomView)
+        {
+        }
+
+        public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+        {
+            bottomView.SetBackgroundResource(Resource.Drawable.bottombackground);
+            bottomView.ItemIconTintList = ColorStateList.ValueOf(Android.Graphics.Color.White);
+
+            bottomView.SetPadding(0, 30, 0, 0);
+
+
+        }
+    }
+}
---
+++ StarbuckXamarin.Android/Effects/MyShellRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using StarbuckXamarin;
+using StarbuckXamarin.Droid.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MainPage), typeof(MyShellRenderer))]
+namespace StarbuckXamarin.Droid.Effects
+{
+    class MyShellRenderer : ShellRenderer
+    {
+        public MyShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new MyShellBottomNavViewAppearanceTracker();
+        }
+    }
+}
