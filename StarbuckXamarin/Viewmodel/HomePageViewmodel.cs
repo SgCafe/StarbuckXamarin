6df176018a41fad0cf2b566a8e6cbd759848d7e7
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-11 18:32:58 -0300

    feat: add radiobuttons and settings to initializate color and prepared the viewmodel to start creating the layout of the items

---
+++ StarbuckXamarin/Viewmodel/HomePageViewmodel.cs
@@ -0,0 +1,65 @@
+﻿using StarbuckXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace StarbuckXamarin.Viewmodel
+{
+    public class HomePageViewmodel : BaseViewmodel
+    {
+        #region properties
+        private ObservableCollection<Product> _coffeeList;
+
+        public ObservableCollection<Product> CoffeeList
+        {
+            get => _coffeeList; 
+            set => SetProperty(ref _coffeeList, value); 
+        }
+
+
+
+        private string _categorySelectorPrimary;
+        public string CategorySelectorPrimary
+        {
+            get => _categorySelectorPrimary;
+            set => SetProperty(ref _categorySelectorPrimary, value);
+        }   
+
+
+        #endregion
+
+        #region constructor
+        public HomePageViewmodel() 
+        {
+            CategorySelectorPrimary = "All";
+            PopulateList();
+        }
+        #endregion
+
+        #region commands
+
+        #endregion
+
+        #region methods
+        private void PopulateList()
+        {
+            CoffeeList = new ObservableCollection<Product>()
+            {
+                new Product
+                {
+                    Name = "Chocolate Frappuccino",
+                    ValueTall = 20.00,
+                    Image = "Brigadeiro.png"
+                },
+                new Product
+                {
+                    Name = "Tea Frappuccino",
+                    ValueTall = 19.00,
+                    Image = "chaVerde.png"
+                }
+            };
+        }
+        #endregion
+    }
+}
--- StarbuckXamarin/Views/HomePage.xaml.cs
+++ StarbuckXamarin/Views/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StarbuckXamarin.Viewmodel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace StarbuckXamarin.Views
         public HomePage()
         {
             InitializeComponent();
+            BindingContext = new HomePageViewmodel();
         }
     }
 }
