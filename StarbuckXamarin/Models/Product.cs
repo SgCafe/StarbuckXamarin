bbe63bbc206aa8d4b11b867023e8ab1afbf59ff6
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-11 18:31:03 -0300

    feat: add models to project

---
+++ StarbuckXamarin/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarbuckXamarin.Models
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double ValueTall { get; set; }
+
+    }
+}
