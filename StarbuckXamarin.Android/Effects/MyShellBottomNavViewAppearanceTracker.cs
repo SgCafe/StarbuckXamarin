34a7cd3118ee7840f8fe6b864a3ea74a6543c9f1
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-10 21:58:32 -0300

    fix: remove class and namespaces unused

--- StarbuckXamarin.Android/Effects/MyShellBottomNavViewAppearanceTracker.cs
+++ StarbuckXamarin.Android/Effects/MyShellBottomNavViewAppearanceTracker.cs
@@ -1,17 +1,7 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.Content.Res;
 using Google.Android.Material.BottomNavigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms;
-using Android.Content.Res;
+using Xamarin.Forms.Platform.Android;
 
 namespace StarbuckXamarin.Droid.Effects
 {
