028558b64d98bc4854dd1e0787502175e265f20c
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-16 19:37:26 -0300

    feat: add viewmodel to detailpage

---
+++ StarbuckXamarin/Viewmodel/DetailPageViewmodel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarbuckXamarin.Viewmodel
+{
+    public class DetailPageViewmodel : BaseViewmodel
+    {
+		private string _sizeSelect;
+
+		public string SizeSelect
+        {
+			get => _sizeSelect; 
+			set => SetProperty(ref _sizeSelect , value); 
+		}
+
+		public DetailPageViewmodel()
+		{
+			SizeSelect = "Tall";
+
+        }
+
+	}
+}
