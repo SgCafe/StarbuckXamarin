ccf60b648f295682aa12ea43d91ed48e3c78a53f
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-16 20:15:49 -0300

    style: change background splash screen

--- StarbuckXamarin.Android/Resources/Resource.designer.cs
+++ StarbuckXamarin.Android/Resources/Resource.designer.cs
@@ -24912,46 +24912,43 @@ namespace StarbuckXamarin.Droid
 			public const int preference_list_divider_material = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int price = 2131165342;
+			public const int search = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int search = 2131165343;
+			public const int shadow_1 = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int shadow_1 = 2131165344;
+			public const int shop_bag = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int shop_bag = 2131165345;
+			public const int splash_screen = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int splash_screen = 2131165346;
+			public const int splash_theme = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int splash_theme = 2131165347;
+			public const int star = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int star = 2131165348;
+			public const int substract = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int substract = 2131165349;
+			public const int tall = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int tall = 2131165350;
+			public const int test_custom_background = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int test_custom_background = 2131165351;
+			public const int tooltip_frame_dark = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int tooltip_frame_dark = 2131165352;
+			public const int tooltip_frame_light = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int tooltip_frame_light = 2131165353;
+			public const int venti = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int venti = 2131165354;
-			
-			// aapt resource value: 0x7F0700AB
-			public const int wallet = 2131165355;
+			public const int wallet = 2131165354;
 			
 			static Drawable()
 			{
