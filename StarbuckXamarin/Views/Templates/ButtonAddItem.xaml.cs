fee2c32ce87986818e3f50798232cf9e16a16c69
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-17 18:46:04 -0300

    feat: finish static detail page

---
+++ StarbuckXamarin/Views/Templates/ButtonAddItem.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StarbuckXamarin.Views.Templates
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ButtonAddItem : Grid
+    {
+        public int quantity = 1;
+
+        public ButtonAddItem ()
+		{
+			InitializeComponent ();
+            quantity = 1;
+            
+
+        }
+
+        private void count_less(object sender, EventArgs e)
+        {
+            if(quantity != 0)
+            {
+                quantity--;
+                txtButton.Text = quantity.ToString();
+            }
+			
+        }
+        private void count_more(object sender, EventArgs e)
+        {
+            quantity++;
+            txtButton.Text = quantity.ToString();
+
+        }
+    }
+}
