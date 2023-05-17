8e14a3a101ff8454fe2953e11c3eff629d416386
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-17 18:56:28 -0300

    feat: add navigation in backbutton to detail page from homepage

--- StarbuckXamarin/Viewmodel/DetailPageViewmodel.cs
+++ StarbuckXamarin/Viewmodel/DetailPageViewmodel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace StarbuckXamarin.Viewmodel
 {
     public class DetailPageViewmodel : BaseViewmodel
     {
-		private string _sizeSelect;
+
+        #region properties
+        private string _sizeSelect;
 
 		public string SizeSelect
         {
@@ -14,11 +18,26 @@ namespace StarbuckXamarin.Viewmodel
 			set => SetProperty(ref _sizeSelect , value); 
 		}
 
-		public DetailPageViewmodel()
+        #endregion
+
+        #region constructor
+        public DetailPageViewmodel()
 		{
 			SizeSelect = "Tall";
+            BackPageButton = new Command(ExecuteBackPageButtonCommand);
 
         }
+        #endregion
 
-	}
+        #region commands
+        public ICommand BackPageButton { get; set; }
+        #endregion
+
+        #region methods
+        private async void ExecuteBackPageButtonCommand()
+        {
+            await Shell.Current.GoToAsync("///homeShell");
+        }
+        #endregion
+    }
 }
