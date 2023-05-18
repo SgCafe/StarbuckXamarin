cf68b9eba6d12cc1f0c1ef316326a6cb909e6942
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-05-17 21:29:24 -0300

    feat: initially add CardPage to the project

---
+++ StarbuckXamarin/Viewmodel/CartPageViewmodel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace StarbuckXamarin.Viewmodel
+{
+    public class CartPageViewmodel : BaseViewmodel
+    {
+        #region properties
+
+        #endregion
+
+        #region constructor
+        public CartPageViewmodel()
+        {
+            BackHomePageCommand = new Command(ExecuteBackHomePageCommand);
+        }
+        #endregion
+
+        #region commands
+        public ICommand BackHomePageCommand { get; set; }
+        #endregion
+
+        #region methods
+        private async void ExecuteBackHomePageCommand()
+        {
+            await Shell.Current.GoToAsync("///homeShell");
+        }
+        #endregion
+
+    }
+}
