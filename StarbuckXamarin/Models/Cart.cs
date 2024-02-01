a8855dd4ce9783ec4efd3704984b7b503881eda1
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2024-02-01 11:11:51 -0300

    refactor: adds features for changing the view

--- StarbuckXamarin/Models/Cart.cs
+++ StarbuckXamarin/Models/Cart.cs
@@ -1,15 +1,29 @@
 ﻿using Newtonsoft.Json;
+using StarbuckXamarin.Viewmodel;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StarbuckXamarin.Models
 {
-    public class Cart
+    public class Cart : BaseViewmodel
     {
+        public string FirebaseKey { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
         public double Price { get; set; }
-        public string Size { get; set; }
+        public   string Size { get; set; }
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity; 
+            set => SetProperty(ref _quantity, value);
+        }
+        private double _totalValue;
+        public double TotalValue
+        {
+            get => _totalValue;
+            set => SetProperty(ref _totalValue, value);
+        }
     }
 }
