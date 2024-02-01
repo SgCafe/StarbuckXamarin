b4fd736ec1f7ee1c063682b3a73b52017a6f4df6
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2024-02-01 11:13:40 -0300

    feat: add new model for address

---
+++ StarbuckXamarin/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarbuckXamarin.Models
+{
+    public class Address
+    {
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+        [JsonProperty("localidade")]
+        public string Cidade { get; set; }
+    }
+}
